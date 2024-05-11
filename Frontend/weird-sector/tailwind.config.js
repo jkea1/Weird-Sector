49e61c421d7be92cddb3f3680c9d58f489fb22f4
jkea1 <jinkyng7890@gmail.com>
2024-05-11 22:19:25 +0900

    feat: 색상 팔레트 설정

--- Frontend/weird-sector/tailwind.config.js
+++ Frontend/weird-sector/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          black: '#000000',
+          white: '#FFFFFF',
+          orange: '#EE3918',
+          grey: '#CFCFCF',
+        },
+        hover: {
+          dark: '#414040',
+          light: '#E1E1E1',
+        },
+      },
+    },
   },
   plugins: [],
 }
