94f347c00d431b58b1a5b01512c77de8b4039a22
jkea1 <jinkyng7890@gmail.com>
2024-05-12 21:45:24 +0900

    style: navbar에 fixed 적용

--- Frontend/weird-sector/src/components/common/Footer.tsx
+++ Frontend/weird-sector/src/components/common/Footer.tsx
@@ -1,30 +1,28 @@
 export default function Footer() {
   return (
-    <footer className='text-gray-800 py-4 h-[330px] border-t pt-12'>
-      <div className='w-full h-full'>
-        <div className='grid grid-cols-3 w-[1140px] h-2/3 m-auto'>
-          <div className='px-4'>
-            <h2 className='font-semibold text-base'>개발자 테스트용</h2>
-          </div>
-          <div className='px-4'>
-            <ul className='mt-2 text-sm'>
-              <li>대표 홍길동 ㅣ 사업자 등록 번호 123-4567-890</li>
-              <li>서울특별시 가가동 나나로 111-2 8층</li>
-              <li>통신 판매업 신고 제 2014-서울홍홍홍 0291호</li>
-            </ul>
-          </div>
-          <div className='pl-40'>
-            <ul className='mt-2 text-sm'>
-              <li>고객지원 111-2345</li>
-              <li>이메일 help@gmail.com</li>
-              <li>평일 09:00 - 17:00 토,일 공휴일 휴무</li>
-            </ul>
-          </div>
+    <footer className='text-gray-800 h-[330px] border-t relative top-[88px]'>
+      <div className='grid grid-cols-3 w-[1140px] h-3/5 mx-auto mt-14'>
+        <div className='px-4'>
+          <h2 className='font-semibold text-base'>개발자 테스트용</h2>
         </div>
-        <div className='text-center text-[#B8B7B7] py-4 text-[10px] border-t mt-4'>
-          <p>이용약관 개인정보처리방침</p>
-          <p>Copyright ©2023 Data nugget. All rights reserved.</p>
+        <div className='px-4'>
+          <ul className='mt-2 text-sm'>
+            <li>대표 홍길동 ㅣ 사업자 등록 번호 123-4567-890</li>
+            <li>서울특별시 가가동 나나로 111-2 8층</li>
+            <li>통신 판매업 신고 제 2014-서울홍홍홍 0291호</li>
+          </ul>
         </div>
+        <div className='pl-40'>
+          <ul className='mt-2 text-sm'>
+            <li>고객지원 111-2345</li>
+            <li>이메일 help@gmail.com</li>
+            <li>평일 09:00 - 17:00 토,일 공휴일 휴무</li>
+          </ul>
+        </div>
+      </div>
+      <div className='text-center text-[#B8B7B7] py-6 text-[10px] border-t mt-4'>
+        <p>이용약관 개인정보처리방침</p>
+        <p>Copyright ©2023 Data nugget. All rights reserved.</p>
       </div>
     </footer>
   )
--- Frontend/weird-sector/src/pages/Auth/Login/Login.tsx
+++ Frontend/weird-sector/src/pages/Auth/Login/Login.tsx
@@ -1,3 +1,38 @@
+import { Link } from 'react-router-dom'
+import Button from '../../../components/common/Button'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className='my-0 h-[711px] flex flex-col justify-center items-center relative top-[88px]'>
+      <h1 className='text-[#040404] text-[32px] font-bold mb-4'>로그인</h1>
+      <div className='flex flex-col gap-y-2'>
+        <input
+          className='w-[383px] h-[52px] border-[1px] rounded border-[#E1E1E1] p-5'
+          type='text'
+          placeholder='이메일 주소'
+        />
+        <input
+          className='w-[383px] h-[52px] border-[1px] rounded border-[#E1E1E1] p-5'
+          type='text'
+          placeholder='비밀번호 입력'
+        />
+      </div>
+      <Button color='black' size='lg' className='my-3'>
+        로그인
+      </Button>
+      <div>
+        <ul className='flex text-sm'>
+          <li className='text-[#808080] border-r-[1.2px] border-[#808080] px-8 my-1'>
+            아이디 찾기
+          </li>
+          <li className='text-[#808080] border-r-[1.2px] border-[#808080] px-8 my-1'>
+            비밀번호 찾기
+          </li>
+          <li className='px-8 my-1'>
+            <Link to='/signUp'>회원가입</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
