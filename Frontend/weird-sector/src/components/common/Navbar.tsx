92c65121a7994369a19466120a7f3021f959f575
jkea1 <jinkyng7890@gmail.com>
2024-05-12 15:09:25 +0900

    feat: Navbar component 생성

---
+++ Frontend/weird-sector/src/components/common/Navbar.tsx
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom'
+import Button from './Button'
+
+type props = {
+  isLoggedIn: boolean
+  userName: string
+}
+
+export default function Navbar({
+  isLoggedIn = true,
+  userName = '정진경',
+}: props) {
+  return (
+    <header className='bg-white h-[88px] border-b border-[#E1E1E1]'>
+      <div className='w-[1140px] h-full flex items-center justify-between m-auto px-4'>
+        <nav className='flex items-center space-x-4'>
+          <Link to='/' className='text-2xl font-bold text-main-orange'>
+            TestSite
+          </Link>
+          <div className='text-[#272727] text-lg'>
+            <Link to='/board/free' className='px-3 py-2 hover:text-[#d6d6d6]'>
+              게시판
+            </Link>
+            <Link to='/dashBoard' className='px-3 py-2 hover:text-[#d6d6d6]'>
+              대시보드
+            </Link>
+          </div>
+        </nav>
+        <div>
+          {isLoggedIn ? (
+            <span className='text-gray-800 text-xl font-bold'>
+              {userName} 님
+            </span>
+          ) : (
+            <Button color='white' size='md'>
+              로그인
+            </Button>
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
