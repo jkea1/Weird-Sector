1b0a5237f8e46f47f9ec3156d746f91c7c16b07c
jkea1 <jinkyng7890@gmail.com>
2024-05-13 16:04:08 +0900

    feat: check box 컴포넌트 생성

---
+++ Frontend/weird-sector/src/components/domain/signUp/Checkbox.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { FaCheck } from 'react-icons/fa'
+
+type Props = {
+  initialChecked?: boolean
+  className?: string
+}
+
+export default function Checkbox({
+  initialChecked = false,
+  className = '',
+}: Props) {
+  const [isChecked, setIsChecked] = useState(initialChecked)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked)
+  }
+
+  return (
+    <div
+      className={`relative w-6 h-6 rounded-[5px] border ${
+        isChecked ? 'bg-main-black' : 'border-[#E1E1E1] bg-[#F9F9F9]'
+      } ${className}`}
+    >
+      <input
+        type='checkbox'
+        checked={isChecked}
+        onChange={handleChange}
+        className='appearance-none absolute w-full h-full cursor-pointer'
+      />
+      {isChecked && (
+        <FaCheck className='absolute m-1 p-[2px] text-main-white pointer-events-none' />
+      )}
+    </div>
+  )
+}
