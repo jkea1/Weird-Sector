1b0150edd259720ac673227f99c2cf3b9470bda2
jkea1 <jinkyng7890@gmail.com>
2024-05-13 16:54:41 +0900

    feat: signUpComplete 페이지 UI 생성

--- Frontend/weird-sector/src/pages/Auth/SignUpComplete/SignUpComplete.tsx
+++ Frontend/weird-sector/src/pages/Auth/SignUpComplete/SignUpComplete.tsx
@@ -1,3 +1,24 @@
+import { AiOutlineCheck } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import Button from '../../../components/common/Button'
+
 export default function SignUpComplete() {
-  return <div>SignUpComplete</div>
+  return (
+    <div className='my-0 h-[1068px] flex flex-col justify-center items-center relative top-[88px]'>
+      <div className='flex flex-col items-center gap-y-3'>
+        <div className='bg-main-orange rounded-full w-10 h-10'>
+          <AiOutlineCheck className='text-main-white w-10 h-10 p-2' />
+        </div>
+        <h1 className='text-2xl text-[#040404] font-bold'>
+          회원가입이 완료되었습니다.
+        </h1>
+        <h3 className='text-xl text-[#808080] font-normal'>축하드려요!</h3>
+      </div>
+      <Link to='/login'>
+        <Button color='black' size='md' className='mt-24'>
+          로그인
+        </Button>
+      </Link>
+    </div>
+  )
 }
