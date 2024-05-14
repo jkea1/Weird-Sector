193a2ae7a2011fd7950f149be0f7fecf943724c2
jkea1 <jinkyng7890@gmail.com>
2024-05-14 15:00:06 +0900

    feat: PostListHeader 생성

---
+++ Frontend/weird-sector/src/components/domain/board/PostListHeader.tsx
@@ -0,0 +1,11 @@
+export default function PostListHeader() {
+  return (
+    <div className='flex items-center w-[1144px] h-[51px] font-bold text-center border-b-[1px] border-b-[#E1E1E1] border-t-[2px] border-t-[#000000]'>
+      <div className='w-[62px]'>No</div>
+      <div className='w-[602px]'>제목</div>
+      <div className='w-[160px]'>글쓴이</div>
+      <div className='w-[160px]'>작성기간</div>
+      <div className='w-[160px]'>조회수</div>
+    </div>
+  )
+}
