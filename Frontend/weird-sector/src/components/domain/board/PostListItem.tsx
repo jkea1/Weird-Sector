acac16c69ce9df1a8b7379ed0a215b21720ecb91
jkea1 <jinkyng7890@gmail.com>
2024-05-14 15:00:37 +0900

    feat: PostListItem 컴포넌트 생성

---
+++ Frontend/weird-sector/src/components/domain/board/PostListItem.tsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom'
+
+type props = {
+  no: string
+  title: string
+  userName: string
+  date: string
+  viewCount: string
+}
+
+export default function PostListItem({
+  no,
+  title,
+  userName,
+  date,
+  viewCount,
+}: props) {
+  return (
+    <Link to='/post/1'>
+      <div className='flex items-center w-[1144px] h-[51px] border-b-[1px] border-b-[#E1E1E1] hover:bg-[#F9F9F9]'>
+        <div className='w-[62px] text-center'>{no}</div>
+        <div className='w-[602px] text-center'>{title}</div>
+        <div className='w-[160px] text-center'>{userName}</div>
+        <div className='w-[160px] text-center'>{date}</div>
+        <div className='w-[160px] text-center'>{viewCount}</div>
+      </div>
+    </Link>
+  )
+}
