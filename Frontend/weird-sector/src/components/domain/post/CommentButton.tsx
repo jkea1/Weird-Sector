a3af25cfb93abd013bbdd5b02a156a7f25fb0d83
jkea1 <jinkyng7890@gmail.com>
2024-05-15 14:40:35 +0900

    style: commentButton 텍스트 크기 수정

--- Frontend/weird-sector/src/components/domain/post/CommentButton.tsx
+++ Frontend/weird-sector/src/components/domain/post/CommentButton.tsx
@@ -1,6 +1,6 @@
 export default function CommentButton() {
   return (
-    <button className='w-[142px] h-[55px] bg-black text-white rounded hover:bg-hover-dark'>
+    <button className='w-[142px] h-[55px] bg-black text-white text-lg rounded hover:bg-hover-dark'>
       댓글작성
     </button>
   )
