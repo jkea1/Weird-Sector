983378854b0882ba06512f0142cf7cddb896f757
jkea1 <jinkyng7890@gmail.com>
2024-05-22 14:20:28 +0900

    feat: 탭 셀렉터 색 변화 로직 추가

--- Frontend/weird-sector/src/components/domain/board/BoardTabSelector.tsx
+++ Frontend/weird-sector/src/components/domain/board/BoardTabSelector.tsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom'
 import Button from '../../common/Button'
 
-export default function BoardTabSelector() {
+type props = {
+  category: string | undefined
+}
+
+export default function BoardTabSelector({ category = 'free' }: props) {
   return (
     <nav className='flex gap-x-3 justify-center mt-14 mb-8'>
       <Link to='/board/free'>
-        <Button color='orange' size='sm'>
+        <Button color={category === 'free' ? 'orange' : 'grey'} size='sm'>
           자유 게시판
         </Button>
       </Link>
       <Link to='/board/question'>
-        <Button color='grey' size='sm'>
+        <Button color={category === 'question' ? 'orange' : 'grey'} size='sm'>
           질문 게시판
         </Button>
       </Link>
       <Link to='/board/etc'>
-        <Button color='grey' size='sm'>
+        <Button color={category === 'etc' ? 'orange' : 'grey'} size='sm'>
           기타 게시판
         </Button>
       </Link>
