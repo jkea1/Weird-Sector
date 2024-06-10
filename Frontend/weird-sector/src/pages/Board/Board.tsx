1f1f862ed252873271862aecdfa13e504094f63f
jkea1 <jinkyng7890@gmail.com>
2024-06-10 20:27:41 +0900

    style: fix 속성 제거

--- Frontend/weird-sector/src/pages/Board/Board.tsx
+++ Frontend/weird-sector/src/pages/Board/Board.tsx
@@ -7,57 +7,6 @@ import PostList from '../../components/domain/board/PostList'
 import { getMeData } from '../../service/auth'
 import { getPosts } from '../../service/posts'
 
-// const postsData = [
-//   {
-//     id: '1',
-//     category: 'free',
-//     title: '새로운 게시물 생성',
-//     text: 'new message :)',
-//     nickname: 'ea',
-//     createdAt: 'Mon May 20 2024 14:49:41 GMT+0900 (Korean Standard Time)',
-//     userId: '1716105098142',
-//     viewCount: '20344',
-//     file: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
-//     hashtag: ['새로운', '게시물', '생성'],
-//     comment: [
-//       {
-//         nickname: '진경',
-//         commentText: '댓글을 생성하다.',
-//         createdAt: 'Mon May 20 2024 14:49:41 GMT+0900 (Korean Standard Time)',
-//       },
-//       {
-//         nickname: '에릭',
-//         commentText: '에릭이 댓글을 생성하다.',
-//         createdAt: 'Mon May 20 2024 14:49:41 GMT+0900 (Korean Standard Time)',
-//       },
-//     ],
-//   },
-//   {
-//     id: '1',
-//     category: 'free',
-//     title: '새로운 게시물 생성',
-//     text: 'new message :)',
-//     nickname: 'ea',
-//     createdAt: 'Mon May 20 2024 14:49:41 GMT+0900 (Korean Standard Time)',
-//     userId: '1716105098142',
-//     viewCount: '20344',
-//     file: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
-//     hashtag: ['새로운', '게시물', '생성'],
-//     comment: [
-//       {
-//         nickname: '진경',
-//         commentText: '댓글을 생성하다.',
-//         createdAt: 'Mon May 20 2024 14:49:41 GMT+0900 (Korean Standard Time)',
-//       },
-//       {
-//         nickname: '에릭',
-//         commentText: '에릭이 댓글을 생성하다.',
-//         createdAt: 'Mon May 20 2024 14:49:41 GMT+0900 (Korean Standard Time)',
-//       },
-//     ],
-//   },
-// ]
-
 export default function Board() {
   const { category } = useParams()
 
@@ -91,14 +40,14 @@ export default function Board() {
   }
 
   return (
-    <div className='my-0 h-[1069px] flex flex-col justify-center items-center relative top-[88px]'>
-      <div className='fixed top-[180px]'>
+    <div className='my-0 h-[1069px] flex flex-col justify-start items-center relative top-[260px]'>
+      <div>
         <h3 className='text-main-orange text-[20px] font-bold text-center'>
           board
         </h3>
         <h1 className='text-[32px] font-bold my-3'>{`${categoryName}게시판`}</h1>
       </div>
-      <div className='fixed top-[300px]'>
+      <div>
         <BoardTabSelector category={category} />
         <div>
           <PostList posts={posts} className='mt-10' />
